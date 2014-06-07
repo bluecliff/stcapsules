06cfb27c1b3b9a0f57767c24c4de15183c0b01bc
lishuangjiang <li0shuangjiang@gmail.com>
2014-06-07 21:55:27 +0800

    release

--- core.py
+++ core.py
@@ -3,4 +3,10 @@
 
 import mongoengine
 
-db=mongoengine.connect('stcapsules',host='127.0.0.1')
+DB_HOST='mongo.duapp.com'#'127.0.0.1'
+DB_NAME='PuypNiLjvatfkUVZDJXB'#'stcapsules'
+DB_PORT=8908
+USER_NAME='Au2wGWafiyKBIDAMik4onX0d'
+PASSWORD='7vaDG1eXI4TkqNTm4EEftkKzetXeudlO'
+
+db=mongoengine.connect(DB_NAME,host=DB_HOST,port=DB_PORT,username=USER_NAME,password=PASSWORD)
